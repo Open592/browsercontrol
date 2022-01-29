1ba96b0a8bc02757645ad4c01451d0863ecb33de
Open592 Developer <uss@open592.com>
2022-01-29 00:38:35 -0800

    all: Add interface and stub
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ src/browsercontrol.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "include/browsercontrol.h"
+
+jboolean Java_nativeadvert_browsercontrol_browsercontrol0(JNIEnv* env, jclass thisObj, jobject parentWindow, jstring url)
+{
+}
+
+void Java_nativeadvert_browsercontrol_destroy0(JNIEnv* env, jclass thisObj)
+{
+}
+
+void Java_nativeadvert_browsercontrol_navigate0(JNIEnv* env, jclass thisObj, jstring url)
+{
+}
+
+void Java_nativeadvert_browsercontrol_resize0(JNIEnv* env, jclass thisObj, jint width, jint height)
+{
+}
