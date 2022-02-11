1980297d633347eb524071bf66883ced201b6edb
Open592 Developer <uss@open592.com>
2022-02-11 11:34:04 -0800

    examples: Copy over jar file to output directory instead of saving path
    Signed-off-by: Open592 Developer <uss@open592.com>

--- examples/ExampleAppletViewer/ExampleAppletViewer.java
+++ examples/ExampleAppletViewer/ExampleAppletViewer.java
@@ -8,42 +8,8 @@ import java.nio.file.Files;
 import nativeadvert.browsercontrol;
 
 public class ExampleAppletViewer {
-    private static String findLibraryPath() {
-        /*
-         * On multi-configuration generators (For example Windows) we will
-         * generate multiple library files. Loop through the possible
-         * library-path-* files. When we successfully load the library return
-         */
-        Path[] libraryPathFileNames = new Path[] {
-            Paths.get("library-path-Debug.txt"),
-            Paths.get("library-path-Release.txt"),
-            Paths.get("library-path-MinSizeRel.txt"),
-            Paths.get("library-path-RelWithDebInfo.txt")
-        };
-
-        for (Path libraryPathFileName : libraryPathFileNames) {
-            try {
-                String libraryPath = new String(Files.readAllBytes(libraryPathFileName));
-
-                System.out.println("Found library path: " + libraryPath);
-
-                if (libraryPath != null && !libraryPath.isEmpty()) {
-                    return libraryPath;
-                }
-            } catch (IOException ignored) {
-                ignored.printStackTrace();
-                continue;
-            }
-        }
-
-        return "";
-    }
     public static void main(String[] args) {
-        String libraryPath = findLibraryPath();
-
-        System.out.println("Found library path: " + libraryPath);
-
-        System.load(libraryPath);
+        System.load("./browsercontrol.dll");
 
         browsercontrol.Test();
     }
