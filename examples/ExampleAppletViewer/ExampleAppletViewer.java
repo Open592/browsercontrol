0b34d03760b86ce184abdf5dcd4c92423d155be1
Open592 Developer <uss@open592.com>
2022-02-11 07:34:34 -0800

    examples: Start loading library within example
    Signed-off-by: Open592 Developer <uss@open592.com>

--- examples/ExampleAppletViewer/ExampleAppletViewer.java
+++ examples/ExampleAppletViewer/ExampleAppletViewer.java
@@ -1,10 +1,49 @@
 package ExampleAppletViewer;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import nativeadvert.browsercontrol;
 
 public class ExampleAppletViewer {
+    private static String findLibraryPath() {
+        /*
+         * On multi-configuration generators (For example Windows) we will
+         * generate multiple library files. Loop through the possible
+         * library-path-* files. When we successfully load the library return
+         */
+        Path[] libraryPathFileNames = new Path[] {
+            Paths.get("library-path-Debug.txt"),
+            Paths.get("library-path-Release.txt"),
+            Paths.get("library-path-MinSizeRel.txt"),
+            Paths.get("library-path-RelWithDebInfo.txt")
+        };
+
+        for (Path libraryPathFileName : libraryPathFileNames) {
+            try {
+                String libraryPath = new String(Files.readAllBytes(libraryPathFileName));
+
+                System.out.println("Found library path: " + libraryPath);
+
+                if (libraryPath != null && !libraryPath.isEmpty()) {
+                    return libraryPath;
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+                continue;
+            }
+        }
+
+        return "";
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String libraryPath = findLibraryPath();
+
+        System.out.println("Found library path: " + libraryPath);
+
+        System.load(libraryPath);
 
         browsercontrol.Test();
     }
