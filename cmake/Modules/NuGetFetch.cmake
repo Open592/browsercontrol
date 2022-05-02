6c0b5d2d34eacfdf0ec903b1ee7754e60872a1dc
Open592 Developer <uss@open592.com>
2022-05-01 23:48:29 -0700

    windows(cmake): Add module to fetch NuGet packages
    Will be used to fetch WebView2 SDK
    
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ cmake/Modules/NuGetFetch.cmake
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Support fetching NuGet packages
+#
+# Added primarily to support fetching WebView2 for Windows platforms
+#
+# OUTPUT_NAME :: CMAKE variable name to associate with this package
+# PACKAGE_NAME :: NuGet package name we want to fetch
+# VERSION :: Version of the package we want to fetch
+function(NuGetFetch OUTPUT_NAME PACKAGE_NAME PACKAGE_VERSION)
+    find_program(NUGET nuget)
+
+    set(NUGET_CLI_EXECUTABLE "nuget-cli.exe")
+    set(NUGET_CLI_PATH "${CMAKE_CURRENT_BINARY_DIR}/tools/${NUGET_CLI_EXECUTABLE}")
+    set(NUGET_PACKAGE_PATH)
+
+    if (NUGET STREQUAL "NUGET-NOTFOUND")
+        if (NOT EXISTS ${NUGET_CLI_PATH})
+            message(STATUS "[NuGet] :: Failed to find NuGet executable. Downloading...")
+            file(DOWNLOAD "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" ${NUGET_CLI_PATH})
+        endif ()
+    endif ()
+
+    execute_process(COMMAND ${NUGET_CLI_PATH} install ${PACKAGE_NAME} -Version ${PACKAGE_VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
+
+    set(${OUTPUT_NAME}-PATH "${CMAKE_CURRENT_BINARY_DIR}/packages/${PACKAGE_NAME}" PARENT_SCOPE)
+endfunction()
