666bba752e88ec1b15017228636e49166caa0e81
Open592 Developer <uss@open592.com>
2022-05-22 23:23:35 -0700

    cmake: Cleanup nuget script
    Signed-off-by: Open592 Developer <uss@open592.com>

--- cmake/Modules/NuGetFetch.cmake
+++ cmake/Modules/NuGetFetch.cmake
@@ -12,16 +12,20 @@ function(NuGetFetch OUTPUT_NAME PACKAGE_NAME PACKAGE_VERSION)
 
     set(NUGET_CLI_EXECUTABLE "nuget-cli.exe")
     set(NUGET_CLI_PATH "${CMAKE_CURRENT_BINARY_DIR}/tools/${NUGET_CLI_EXECUTABLE}")
-    set(NUGET_PACKAGE_PATH)
+    set(NUGET_EXECUTABLE)
 
     if (NUGET STREQUAL "NUGET-NOTFOUND")
         if (NOT EXISTS ${NUGET_CLI_PATH})
             message(STATUS "[NuGet] :: Failed to find NuGet executable. Downloading...")
             file(DOWNLOAD "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" ${NUGET_CLI_PATH})
         endif ()
+
+        set(NUGET_EXECUTABLE ${NUGET_CLI_PATH})
+    elseif ()
+        set(NUGET_EXECUTABLE ${NUGET})
     endif ()
 
-    execute_process(COMMAND ${NUGET_CLI_PATH} install ${PACKAGE_NAME} -Version ${PACKAGE_VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
+    execute_process(COMMAND ${NUGET_EXECUTABLE} install ${PACKAGE_NAME} -Version ${PACKAGE_VERSION} -ExcludeVersion -OutputDirectory ${CMAKE_CURRENT_BINARY_DIR}/packages OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
 
     set(${OUTPUT_NAME}-PATH "${CMAKE_CURRENT_BINARY_DIR}/packages/${PACKAGE_NAME}" PARENT_SCOPE)
 endfunction()
