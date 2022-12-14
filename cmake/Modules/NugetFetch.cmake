bf8eac4c74672275f91d4435f86708514472a40b
Open592 Developer <uss@open592.com>
2022-12-13 21:36:22 -0800

    linux: Add logic to locate/fetch CEF
    Signed-off-by: Open592 Developer <uss@open592.com>

--- cmake/Modules/NugetFetch.cmake
+++ cmake/Modules/NugetFetch.cmake
@@ -9,7 +9,7 @@
 # PACKAGE_NAME :: NuGet package name we want to fetch
 #
 # VERSION :: Version of the package we want to fetch
-function(nugetfetch OUTPUT_NAME PACKAGE_NAME PACKAGE_VERSION)
+function(NugetFetch OUTPUT_NAME PACKAGE_NAME PACKAGE_VERSION)
   find_program(NUGET_EXE nuget)
 
   if(NOT NUGET_EXE)
