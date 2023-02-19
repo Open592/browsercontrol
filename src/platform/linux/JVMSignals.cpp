b9bb5501255d8246be1ea0d933bc03b116b2aeb1
Open592 Developer <uss@open592.com>
2023-02-19 02:48:56 -0800

    linux: Save/Restore JVM signal handlers
    This is required to prevent random crashes due to CEF overwriting the
    signal handlers the JVM is setting.
    
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ src/platform/linux/JVMSignals.cpp
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include <array>
+#include <csignal>
+
+#include "JVMSignals.hpp"
+
+namespace {
+
+// List of signals which the JVM set, which we want to save and after initializing
+// CEF, restore.
+//
+// https://github.com/chromiumembedded/java-cef/blob/master/native/signal_restore_posix.cpp
+const std::array<int, 13> signalsToRestore = {
+    SIGHUP,
+    SIGINT,
+    SIGQUIT,
+    SIGILL,
+    SIGABRT,
+    SIGFPE,
+    SIGSEGV,
+    SIGALRM,
+    SIGTERM,
+    SIGCHLD,
+    SIGBUS,
+    SIGTRAP,
+    SIGPIPE,
+};
+
+std::array<struct sigaction, signalsToRestore.size()> savedActions;
+
+}
+
+namespace JVMSignals {
+
+void Backup()
+{
+    struct sigaction action { };
+
+    for (size_t i = 0; i < signalsToRestore.size(); ++i) {
+        memset(&action, 0, sizeof(action));
+
+        sigaction(signalsToRestore[i], nullptr, &action);
+
+        savedActions[i] = action;
+    }
+}
+
+void Restore()
+{
+    for (size_t i = 0; i < savedActions.size(); ++i) {
+        sigaction(signalsToRestore[i], &savedActions[i], nullptr);
+    }
+}
+
+}
