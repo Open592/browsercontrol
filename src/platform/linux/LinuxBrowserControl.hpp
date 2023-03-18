f84730fc1bd9478d015f699d8b9a092aa2f80799
Open592 Developer <uss@open592.com>
2023-03-18 12:31:59 -0700

    linux: Blow away linux implementation so we can restart
    I think we need a fresh start after all the work we have done. I read
    through a lot of CEF code and think we can clean everything up and end
    up with something much nicer.
    
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/platform/linux/LinuxBrowserControl.hpp
+++ src/platform/linux/LinuxBrowserControl.hpp
@@ -2,19 +2,10 @@
 
 #pragma once
 
-#include <filesystem>
 #include <memory>
-
-#include <include/base/cef_thread_checker.h>
-#include <include/cef_base.h>
-#include <jawt.h>
-#include <jni.h>
-#include <xcb/xcb.h>
 
 #include "src/AbstractBrowserControl.hpp"
 #include "src/BrowserData.hpp"
-
-#include "BrowserControlApp.hpp"
 
 class LinuxBrowserControl : public AbstractBrowserControl {
 public:
@@ -28,22 +19,5 @@ public:
     void Navigate(std::wstring) noexcept override;
 
 private:
-    [[nodiscard]] static CefWindowHandle ResolveHostWindow(JNIEnv*, jobject) noexcept;
-
-    /**
-     * We require that our host process passes us information about the current working
-     * directory of the executable. We use this information to pass the browser subprocess
-     * helper path to chromium.
-     *
-     * This information is passed in Java system properties under the following key:
-     *
-     * com.open592.workingDirectory
-     *
-     * We require that the browser helper is a sibling of the applet viewer.
-     */
-    [[nodiscard]] static std::filesystem::path ResolveWorkingDirectory(JNIEnv*) noexcept;
-
-    CefRefPtr<BrowserControlApp> m_app;
     std::shared_ptr<BrowserData> m_data;
-    base::ThreadChecker m_threadChecker;
 };
